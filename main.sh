de84b6364da2eab870f0455fd04ac5bec79b91a8
michaelmouton <michael.mouton510@gmail.com>
2022-06-14 13:38:31 +0200

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo mkdir /usr/share/fonts/ubuntu-font-family
+sudo cp ./ubuntu-font-family/*.ttf /usr/share/fonts/ubuntu-font-family
+sudo chmod -R 777 /usr/share/fonts/ubuntu-font-family
+sudo fc-cache -fv
